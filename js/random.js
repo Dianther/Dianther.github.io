4413a46d00cd8c74a88d03acef5700f60f1d302d
xym <1152055859@qq.com>
2025-08-20 19:12:14 +0800

    Site updated: 2025-08-20 19:12:13

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["2025/08/20/hello-world/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2025/08/20/hello-world/","2025/01/20/我的第一篇博客文章/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
